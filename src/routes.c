462b1dcde3dbe285a18ea8585353b37fbaa7bdd3
halosghost <halosghost@archlinux.info>
2016-11-09 18:43:16 -0600

    get it to compile

--- src/routes.c
+++ src/routes.c
@@ -3,18 +3,16 @@
 static signed
 proj_list_gen (coro_t * coro) {
 
-    struct project * proj = coro_get_data(coro);
+    struct projects * proj = coro_get_data(coro);
 
     size_t len = sizeof proj_list / sizeof(struct project);
     for ( size_t i = 0; i < len; ++ i ) {
-        proj->name = proj_list[i].name;
-        proj->url  = proj_list[i].url;
-        proj->desc = proj_list[i].desc;
+        proj->project.name = proj_list[i].name;
+        proj->project.url  = proj_list[i].url;
+        proj->project.desc = proj_list[i].desc;
 
         if ( coro_yield(coro, 1) ) { break; }
-    }
-
-    return 0;
+    } return 0;
 }
 
 lwan_http_status_t
@@ -23,11 +21,11 @@ root_handler (lwan_request_t * req, lwan_response_t * resp, void * data) {
     (void )req; (void )data;
 
     const lwan_var_descriptor_t tpl_desc [] = {
-        TPL_VAR_SEQUENCE(struct project, proj_list, proj_list_gen, (
+        TPL_VAR_SEQUENCE(struct projects, project, proj_list_gen, (
             (const lwan_var_descriptor_t [] ){
-                TPL_VAR_STR(struct project, name),
-                TPL_VAR_STR(struct project, url),
-                TPL_VAR_STR(struct project, desc),
+                TPL_VAR_STR(struct projects, project.name),
+                TPL_VAR_STR(struct projects, project.url),
+                TPL_VAR_STR(struct projects, project.desc),
                 TPL_VAR_SENTINEL
             }
         )),
@@ -35,7 +33,7 @@ root_handler (lwan_request_t * req, lwan_response_t * resp, void * data) {
     };
 
     lwan_tpl_t * tpl = lwan_tpl_compile_file("index.htm.mustache", tpl_desc);
-    lwan_tpl_apply_with_buffer(tpl, resp->buffer, proj_list);
+    lwan_tpl_apply_with_buffer(tpl, resp->buffer, &proj_list);
     resp->mime_type = "text/html";
     lwan_tpl_free(tpl);
 
--- src/routes.h
+++ src/routes.h
@@ -10,17 +10,22 @@ lwan_http_status_t
 root_handler (lwan_request_t *, lwan_response_t *, void *);
 
 struct project {
+    lwan_tpl_list_generator_t generator;
     const char * name, * url, * desc;
 };
 
+struct projects {
+    struct project project;
+};
+
 static struct project proj_list [] = {
-    { "shaman",
-      "https://github.com/HalosGhost/shaman",
-      "a simple CLI weather program",
+    { .name = "shaman"
+    , .url  = "https://github.com/HalosGhost/shaman"
+    , .desc = "a simple CLI weather program"
     },
-    { "pbpst",
-      "https://github.com/HalosGhost/pbpst",
-      "a simple and featureful client for pb deployements",
+    { .name = "pbpst"
+    , .url  = "https://github.com/HalosGhost/pbpst"
+    , .desc = "a simple and featureful client for pb deployements"
     }
 };
 
