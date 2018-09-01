7fe8189598e48630261f8e6c7c64b2767702a029
halosghost <samuel.stuewe@gmail.com>
2018-08-31 19:01:02 -0500

    preliminary redirector implementation

--- src/main.h
+++ src/main.h
@@ -4,6 +4,7 @@
 #include <lwan/lwan.h>
 #include <lwan/lwan-template.h>
 #include <lwan/lwan-mod-serve-files.h>
+#include <lwan/lwan-mod-redirect.h>
 #include <time.h>
 #include <stdlib.h>
 #include <limits.h>
---
+++ src/redirector.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+signed
+main (void) {
+
+    struct lwan l;
+    lwan_init(&l);
+
+    #define ACMEDIR "/.well-known/acme-challenge"
+
+    const struct lwan_url_map default_map [] = {
+        { .prefix = ACMEDIR, PRIV_SERVE_FILES("." ACMEDIR) },
+        { .prefix = "/",  REDIRECT_CODE("https://halosgho.st", HTTP_TEMPORARY_REDIRECT) },
+        { .prefix = NULL }
+    };
+
+    lwan_set_url_map(&l, default_map);
+    lwan_main_loop(&l);
+    lwan_shutdown(&l);
+
+    return EXIT_SUCCESS;
+}
