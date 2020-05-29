28c8fc938676012c111eb5bdfa5199dcc7d48a07
halosghost <samuel.stuewe@gmail.com>
2020-05-29 18:55:13 -0500

    Update PRIV_SERVE_FILES for compatibility with latest lwan

--- src/common.h
+++ src/common.h
@@ -16,11 +16,14 @@
 
 #define PRIV_SERVE_FILES(path) \
     .module = LWAN_MODULE_REF(serve_files), \
-    .args = &(struct lwan_serve_files_settings ){ \
+    .args = ((struct lwan_serve_files_settings[]) {{ \
         .root_path = path, \
         .serve_precompressed_files = true, \
-        .auto_index = false \
-    } \
+        .auto_index = false, \
+        .auto_index_readme = false, \
+        .cache_for = SERVE_FILES_CACHE_FOR, \
+    }}), \
+    .flags = (enum lwan_handler_flags)0
 
 #define WEBDIR "/srv/http"
 
